13cb5a94fab062e4adec5a2e6d9b81e036c50c8e
pgk95 <kalupgk@gmail.com>
2023-05-03 06:33:33 -0700

    quick fix

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,7 +1,5 @@
 #!/usr/bin/env ruby
 
-regex = Regexp.new("hbt{2,1,5}n")
-match_data = regex.match(ARGV[0])
-if match_data
-  puts match_data.to_s
+if ARGV[0]
+  puts ARGV[0].scan(/hbt{2,1,5}/).join
 end
