9de74124f081b8e23a1dbc27f0fe9023514b3d73
pgk95 <kalupgk@gmail.com>
2023-06-23 03:02:46 -0700

    task 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,8 @@
+# this puppet kills process named "killmenow".
+
+exec { 'killmenow':
+  command     => 'pkill -f killmenow',
+  path        => '/usr/bin:/usr/sbin:/bin:/sbin',
+  onlyif      => 'pgrep -f killmenow',
+  refreshonly => true,
+}
