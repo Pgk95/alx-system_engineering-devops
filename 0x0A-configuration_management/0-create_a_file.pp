f858f0abd4bb1a8e64054fd8142355ed9a6b7a42
pgk95 <kalupgk@gmail.com>
2023-06-23 02:40:16 -0700

    quick fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # this Puppet code creates file in '/tmp/school' containing the string.
 
-file { '/tmp/school':
-  ensure  => file,
+file { 'school':
+  ensure  => 'present',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => URI.encode_www_form_component('I love Puppet'),
+  content => 'I love Puppet',
+  path    => '/tmp/school'
 }
