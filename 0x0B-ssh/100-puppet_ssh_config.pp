ad2713c1cecdf4348cc31ac14a7f9f67e0d3982d
pgk95 <kalupgk@gmail.com>
2023-07-06 06:46:58 -0700

    quick fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,8 @@
 # this puppet script let you connect to a server without typing a password
 file { '/root/.ssh/config':
   ensure  => present,
-  content => "Host your_server\n\
-              HostName 54.172.106.217\n\
-              User ubuntu\n\
+  content => "Host 54.237.51.224\n\
               IdentityFile ~/.ssh/school\n\
               PreferredAuthentications publickey\n\
               PasswordAuthentication no\n",
-  mode    => '0600',
-  owner   => 'root',
-  group   => 'root',
 }
-
