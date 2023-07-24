19a231279e5c1b947ce2dd50209b47f75fd1c218
pgk95 <pgkkalu@gmail.com>
2023-07-24 10:31:10 -0700

    quick fix and updated code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,43 +1,14 @@
 #!/usr/bin/python3
-
-"""By using the provided REST API returns info about his/her TODO lists"""
-
+"""Returns list of information for a given employee"""
 import requests
-
-
-def get_employee_todo_progress(employee_id):
-    base_url = "https://jsonplaceholder.typicode.com"
-    user_url = f"{base_url}/users/{employee_id}"
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-
-    try:
-        user_response = requests.get(user_url)
-        todos_response = requests.get(todos_url)
-
-        user_data = user_response.json()
-        todos_data = todos_response.json()
-
-        e_name = user_data['name']
-        total_tasks = len(todos_data)
-        done_tasks = sum(1 for todo in todos_data if todo['completed'])
-
-        print(f"Employee {e_name} is done with "
-              f"tasks({done_tasks}/{total_tasks}): ")
-
-        for todo in todos_data:
-            if todo['completed']:
-                print(f"\t{todo['title']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return
-
+import sys
 
 if __name__ == "__main__":
-    import sys
+    url = "https://jsonplaceholder.typicode.com/"
+    employee = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    if len(sys.argv) != 2:
-        print("Usage: python script.py employee_id")
-    else:
-        employee_id = int(sys.argv[1])
-        get_employee_todo_progress(employee_id)
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]
