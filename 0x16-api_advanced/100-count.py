d3c5a5fd09566d9c57579701a5737c1e7a784760
pgk95 <pgkkalu@gmail.com>
2023-08-09 01:14:39 -0700

    quick fix

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,31 +16,27 @@ def count_words(subreddit, word_list, instances={}, after="", count=0):
         if response.status_code == 404:
             raise Exception
     except Exception:
-            print('')
-            return
+        print("")
+        return
 
-    results = results.get('data')
-    after = results.get('after')
-    count += results.get('dist')
-
-    for post in results.get('children'):
-        title = post.get('data').get('title').lower().split()
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
         for word in word_list:
             if word.lower() in title:
-                times = title.count(word.lower())
+                times = len([t for t in title if t == word.lower()])
                 if instances.get(word) is None:
                     instances[word] = times
                 else:
                     instances[word] += times
 
     if after is None:
-        if not instances:
-            print('')
+        if len(instances) == 0:
+            print("")
             return
-
-        sorted_instances = sorted(instances.items(),
-                                  key=lambda kv: (-kv[1], kv[0]))
-        for word, count in sorted_instances:
-            print(f'{word}: {count}')
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
         count_words(subreddit, word_list, instances, after, count)
