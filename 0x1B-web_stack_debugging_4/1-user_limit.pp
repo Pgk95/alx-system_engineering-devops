3ae51c894bcff530f2c8dcafd1bffb96bc430831
pgk95 <pgkkalu@gmail.com>
2023-08-17 07:52:26 -0700

    task both adv and mand

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,11 @@
+# This manifest abes you to login with a user and open a file.
+
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i '/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i '/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
