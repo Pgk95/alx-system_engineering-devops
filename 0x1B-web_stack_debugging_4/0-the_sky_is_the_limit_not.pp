5072e02d32f066bdfd21d8699c06afe740a9aa26
Pgk95 <117736725+Pgk95@users.noreply.github.com>
2023-08-19 22:01:32 +0300

    Update 0-the_sky_is_the_limit_not.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 exec { 'Increase':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-}
+} ->
 
 exec { 'Restart':
   command => 'sudo service nginx restart',
