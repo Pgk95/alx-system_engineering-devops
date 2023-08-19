c8cb5546e21c1cf3650ef878a361594171e1298a
pgk95 <pgkkalu@gmail.com>
2023-08-19 12:06:38 -0700

    quick fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,9 +2,9 @@
 exec { 'Increase':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
-} ->
+}
 
 exec { 'Restart':
-  command => 'sudo service nginx restart',
+  command => 'nginx restart',
   path    => '/etc/init.d/'
 }
