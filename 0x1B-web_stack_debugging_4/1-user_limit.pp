541a4343dc5a9ad5f7a5c87fbf1004da7099b025
pgk95 <pgkkalu@gmail.com>
2023-08-19 11:43:58 -0700

    quick fix on adv task

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,11 +1,13 @@
 # This manifest ables fixing high demand of files.
 
-exec { 'Change':
-  command => 'sudo sed -i 's/nofile 7/nofile 70000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+  unless  => 'grep -q "^holberton hard.*50000" /etc/security/limits.conf',
 }
 
-exec { 'Replace':
-  command => 'sudo sed -i 's/nofile 6/nofile 60000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/',
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+  unless  => 'grep -q "^holberton soft.*50000" /etc/security/limits.conf',
 }
